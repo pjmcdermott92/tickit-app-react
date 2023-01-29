9a63ac22173c5e6fed1c48f18aac4e9855760b00
pjmcdermott92 <patrick@pjmcdermott.com>
2023-01-28 20:19:24 -0700

    add sidebar shrink effects and items

---
+++ src/components/SidebarMenuItem/SidebarMenuItem.tsx
@@ -0,0 +1,52 @@
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import useToggle from '../../hooks/useToggle';
+import './SidebarMenuItem.scss';
+
+type SidebarMenuProps = {
+	isSmall: boolean;
+	label: string;
+	icon: React.Component | any;
+	items?: any[];
+};
+
+const SidebarMenuItem = ({
+	isSmall,
+	label,
+	icon: Icon,
+	items,
+}: SidebarMenuProps) => {
+	const [active, toggleActive] = useToggle(false);
+
+	return (
+		<li className={`sidebar-menu-item`}>
+			<button
+				className={`menu-item ${active ? 'active' : ''} ${
+					items?.length ? 'submenu' : ''
+				}`}
+				onClick={() => toggleActive()}
+			>
+				<div className='icon'>
+					<Icon />
+				</div>
+				<div className='label'>{label}</div>
+			</button>
+			{items?.length && active && (
+				<ul className='submenu'>
+					{items.map(({ label, icon: Icon, href }) => (
+						<li key={label}>
+							<button>
+								<span className='icon'>
+									<Icon />
+								</span>
+								<span className='label'>{label}</span>
+							</button>
+						</li>
+					))}
+				</ul>
+			)}
+		</li>
+	);
+};
+
+export default SidebarMenuItem;
--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,22 +1,99 @@
 import useToggle from '../../../hooks/useToggle';
-import { IoArrowBackCircleOutline } from 'react-icons/io5';
+import * as Icons from 'react-icons/io5';
+import {
+	IoArrowBackCircleOutline,
+	IoPeopleCircleOutline,
+	IoPeopleOutline,
+	IoPersonAddOutline,
+	IoAlbumsSharp,
+	IoAlbumsOutline,
+	IoAddSharp,
+	IoTicketOutline,
+	IoTicketSharp,
+} from 'react-icons/io5';
 import './Sidebar.scss';
+import SidebarMenuItem from '../../SidebarMenuItem/SidebarMenuItem';
+
+const SIDEBAR_MENUS = [
+	{
+		label: 'Users',
+		icon: IoPeopleCircleOutline,
+		items: [
+			{
+				label: 'All Users',
+				icon: IoPeopleOutline,
+				href: '/users',
+			},
+			{
+				label: 'Invite User',
+				icon: IoPersonAddOutline,
+				href: '/users/new',
+			},
+		],
+	},
+	{
+		label: 'Projects',
+		icon: IoAlbumsOutline,
+		items: [
+			{
+				label: 'All Projects',
+				icon: IoAlbumsSharp,
+				href: '/projects',
+			},
+			{
+				label: 'My Projects',
+				icon: IoAlbumsOutline,
+				href: '/projects/assigned',
+			},
+			{
+				label: 'New Project',
+				icon: IoAddSharp,
+				href: '/projects/create',
+			},
+		],
+	},
+	{
+		label: 'Tickets',
+		icon: IoTicketOutline,
+		items: [
+			{
+				label: 'All Tickets',
+				icon: IoTicketSharp,
+				href: '/tickets',
+			},
+			{
+				label: 'My Tickets',
+				icon: IoTicketOutline,
+				href: '/projects/assigned',
+			},
+			{
+				label: 'New Ticket',
+				icon: IoAddSharp,
+				href: '/projects/create',
+			},
+		],
+	},
+];
 
 const Sidebar = () => {
-    const [showSidebar, toggleShowSidebar] = useToggle(true);
+	const [smallSidebar, toggleSmallSidebar] = useToggle(false);
 
 	return (
-		<aside className={`sidebar-container ${!showSidebar ? 'small': ''}`}>
+		<aside className={`sidebar-container ${smallSidebar ? 'small' : ''}`}>
 			<div
 				role='button'
 				className='hide-nav-btn'
 				title='Hide Navbar'
 				tabIndex={0}
-                onClick={() => toggleShowSidebar()}
+				onClick={() => toggleSmallSidebar()}
 			>
 				<IoArrowBackCircleOutline />
 			</div>
-			mf
+			<ul className='sidebar-menu'>
+				{SIDEBAR_MENUS.map((item) => (
+					<SidebarMenuItem key={item.label} isSmall={smallSidebar} {...item} />
+				))}
+			</ul>
 			<span className='sidebar-bottom-bg' />
 		</aside>
 	);
